1b9046542ea5d3c459b212f59aeb8fb9351e114a
summerchoco <shkkk90daum.net>
2024-09-09 12:31:22 +0900

    update + main 합치기

--- chap08-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
+++ chap08-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
@@ -5,16 +5,15 @@ import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
 import com.ohgiraffers.crud.menu.model.service.MenuService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/menu")
